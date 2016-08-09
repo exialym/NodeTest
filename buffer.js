ddf4bf6395f4df182f9d819ceaa1f53dd3a8f443
exialym <exialym@icloud.com>
2016-08-09 16:24:44 +0800

    Buffer 完结

--- buffer.js
+++ buffer.js
@@ -11,14 +11,29 @@ buf1[0] = 1.69;
 console.log(buf1[0]); //1
 
 //解决Buffer拼接的问题
-var chunks = []; 
-var size = 0; 
-res.on('data', function (chunk) {   
-	chunks.push(chunk);   
-	size += chunk.length; 
-}); 
-res.on('end', function () { 
-	var buf = Buffer.concat(chunks, size);   
-	var str = iconv.decode(buf, 'utf8');   
-	console.log(str); 
-}); 
+// var chunks = []; 
+// var size = 0; 
+// res.on('data', function (chunk) {   
+// 	chunks.push(chunk);   
+// 	size += chunk.length; 
+// }); 
+// res.on('end', function () { 
+// 	var buf = Buffer.concat(chunks, size);   
+// 	var str = iconv.decode(buf, 'utf8');   
+// 	console.log(str); 
+// }); 
+
+//buffer与性能
+//这里注释掉的这句话，预先将一个字符串转换为了Buffer
+//这会有效的减少CPU的重复使用，节省服务器的资源。
+//如果进行AB测试的话你会发现读取速率和每秒查询次数均有提高
+var http = require('http'); 
+var helloworld = ""; 
+for (var i = 0; i < 1024 * 10; i++) {   
+	helloworld += "a"; 
+}  
+// helloworld = new Buffer(helloworld);  
+http.createServer(function (req, res) {   
+	res.writeHead(200);   
+	res.end(helloworld); 
+}).listen(8001);
