e6ff5c99fc7733c90b25d501b73ca9919739a778
exialym <exialym@icloud.com>
2016-08-10 15:36:11 +0800

    tcp echo服务器

--- tcp.js
+++ tcp.js
@@ -16,3 +16,11 @@ server.on('connection',function(){
 server.listen(8124, function () {   
 	console.log('server bound'); 
 });
+
+var echoServer = net.createServer(function (socket) {   
+	socket.write('Echo server\r\n');   
+	socket.pipe(socket); 
+});  
+echoServer.listen(1337, function () {   
+	console.log('echoServer bound'); 
+});
