3f0d84cd5fd8f9e237f6d7d075623ebae44e5faa
exialym <exialym@icloud.com>
2016-08-22 17:28:13 +0800

    句柄 测试 还是同一个子进程在处理啊

--- jubing-child.js
+++ jubing-child.js
@@ -1,7 +1,8 @@
 var http = require('http'); 
+var a = Math.round((1 + Math.random()) * 1000);
 var server = http.createServer(function (req, res) {   
 	res.writeHead(200, {'Content-Type': 'text/plain'});   
-	res.end('handled by child, pid is ' + process.pid + '\n'); 
+	res.end('handled by child, pid is ' + process.pid + '\n'+a); 
 });  
 process.on('message', function (m, tcp) {   
 	if (m === 'server') {     
